b47ff99fb12e6443146b40d854c1eca19e8f9b11
halnik111 <halonikolas@gmail.com>
2025-08-10 21:22:04 +0200

    Added timestamps for Order.js to track creation > serving > close duration, editing existing order now edits the order instead of creating a new one every time, added date converter local time to every DB model.

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,13 @@
 import express from "express";
 import {authorizeRoles, verifyToken} from "../middleware/verifyToken.js";
-import {getAllActiveOrders, getOrder, newOrder} from "../controllers/order.js";
+import {
+    closeOrder,
+    editOrder,
+    getAllActiveOrders,
+    getOrder,
+    newOrder,
+    serveOrder,
+} from "../controllers/order.js";
 
 
 const router = express.Router();
@@ -8,4 +15,7 @@ const router = express.Router();
 router.get('/getOrder/:id', verifyToken, authorizeRoles('User', "Manager", "Admin"), getOrder);
 router.post('/newOrder', verifyToken, authorizeRoles('User', "Manager", "Admin"), newOrder);
 router.post('/getAllActiveOrders', verifyToken, authorizeRoles('User', "Manager", "Admin"), getAllActiveOrders);
+router.put('/editOrder/:id', verifyToken, authorizeRoles('User', "Manager", "Admin"), editOrder);
+router.put('/closeOrder/:id', verifyToken, authorizeRoles('User', "Manager", "Admin"), closeOrder);
+router.put('/serveOrder/:id', verifyToken, authorizeRoles('User', "Manager", "Admin"), serveOrder);
 export default router;
